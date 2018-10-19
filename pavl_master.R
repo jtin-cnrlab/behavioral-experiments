ca2cc730cfcdf88d27fc935d9551a29a9163e247
jtin-cnrlab <42445960+jtin-cnrlab@users.noreply.github.com>
2018-10-19 17:18:18 -0400

    Add run number to master CSV
    Use participant and date to identify 1st and 2nd runs

--- pavl_master.R
+++ pavl_master.R
@@ -1,41 +1,32 @@
 # pavl_master.R
-# author: Jessica Tin
-# updated: 3 Aug 2018
+# Jessica Tin
+# 19 Oct 2018
 #
 # Compiles data from every CSV in PAVL's data folder into a single master CSV.
 #
 
+#### SET SERVER PATH ####
+PerrachioneLab <- "/Volumes/PerrachioneLab" # Mac
+#PerrachioneLab <- "/PerrachioneLab" # Linux
+#PerrachioneLab <- "R:/PerrachioneLab" # Windows
+setwd(file.path(PerrachioneLab, "projects", "PAVL"))
+
 #### LOAD PACKAGES ####
-# script uses tidyverse packages dplyr, readr, tidyr (requires R version >= 3.1.2)
-# see dplyr.tidyverse.org (for mutate(), group_by(), etc.) and help(magrittr) (for %>%)
-if(!require("dplyr")) install.packages("dplyr", dependencies = TRUE)
-if(!require("readr")) install.packages("readr", dependencies = TRUE)
-if(!require("stringr")) install.packages("readr", dependencies = TRUE)
-library("dplyr") # for data frame manipulation and forward pipe (%>%)
-library("readr") # for faster reading and writing
-library("stringr") # for str_to_upper()
-
-#### SET WORKING DIRECTORY ####
-# set wd to main project folder
-# (if server is mounted from parent directory, prepend path accordingly, e.g.
-# /Perrachione/PerrachioneLab/.. or sar-research/Perrachione/PerrachioneLab/..)
-setwd("/Volumes/PerrachioneLab/projects/PAVL/") # Mac
-#setwd("/PerrachioneLab/projects/PAVL") # Linux
-#setwd("R:/PerrachioneLab/projects/PAVL") # Windows
-
+source(file.path(PerrachioneLab, "software", "r-scripts", "load_packages.R"))
+load_packages("dplyr", "magrittr", "readr", "stringr")
 
 #### IMPORT INDIVIDUAL CSVs ####
-# create list to save all individual CSV data frames to
-master_list <- c()
-
 # find all CSV files starting with p####_ within project folder > Experiment > data
 # (default location for psychopy output; change file.path if CSVs are elsewhere)
 csv_files <- list.files(path = file.path("Experiment","data"),
                         pattern = "^p[0-9]{4}_.*\\.csv$",
                         full.names = TRUE)
 
+# preallocate list to save all individual CSV data to
+master_list <- vector("list", length = length(csv_files))
+
 # loop through each CSV file
-for(f in csv_files) {
+for (f in csv_files) {
     print(paste0("[",match(f, csv_files),"/",length(csv_files),"] ",f))
 
     # read in CSV as data frame
@@ -78,9 +69,10 @@ for(f in csv_files) {
             order = ifelse(substr(condition, 1, 2) == substr(condition, 4, 5),
                            "SAME", "DIFF"),
 
-            # add block number for trials 201-250
-            block = ifelse(trial > 200, ((trial - 201) %/% 10) + 1, NA)
-
+            # add block number by 40s for trials 1-200, by 10s for trials 201-250
+            block = ifelse(trial <= 200,
+                           ((trial - 1) %/% 40) + 1,
+                           ((trial - 201) %/% 10) + 1)
         ) %>%
 
         # sum up accurate trials
@@ -97,17 +89,26 @@ for(f in csv_files) {
 
 #### COMPILE AND SAVE MASTER CSV ####
     # add this CSV data to the master list
-    master_list <- c(master_list, list(df_csv))
+    master_list[match(f, csv_files)] <- list(df_csv)
 }
-
 # create master data frame from master_list data frames
-master_df <- bind_rows(master_list) %>%
+master_df <- bind_rows(master_list)
+
+# create dated list of participants to determine run order
+dates <- master_df %>%
+    ungroup() %>%
+    group_by(participant, condition) %>%
+    summarize(date = unique(date)) %>%
+    arrange(participant, date)
+dates$run <- rep(c(1,2), length(unique(dates$participant)))
+
+master_df %<>% left_join(dates) %>% # add run info to master
     # reorder columns
     select(participant, trial, talker,
            exposure_response, exposure_accuracy, exposure_acc_sum, exposure_rt,
            test_response, test_accuracy, test_acc_sum, test_rt,
            block, block_acc_sum,
-           condition, experiment, order, date) %>%
+           condition, experiment, order, run, date) %>%
 
     # sort by date
     arrange(date) %>%
